2c7084f25b27d2296156e960d9a34f87515d316d
kanhacoder <88435088+kanhacoder@users.noreply.github.com>
2022-01-29 10:57:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball,ground,wall1,wall2;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+   
+  
+  
+
+  engine = Engine.create();    
+  world = engine.world;
+// 	//Create the Bodies Here.
+//  ball.body = bodies.circle(5,5,10,ball_options);
+  ball = new Ball(200,200,30);
+  ground = new Ground(400,680,800,20);
+	wall1 = new Wall1(450,610,15,120);
+  wall2 = new Wall2(700,610,15,120);
+}
+
+
+function draw() {
+  background("black");
+  Engine.update(engine);
+  ball.display();
+  ground.display();
+  wall1.display();
+  wall2.display();
+}
+
+function keyPressed()
+{
+  if(keyCode === UP_ARROW)
+  {
+    Body.applyForce(ball.body,ball.body.position,{x:100,y:14})      
+  }
+}
+
+
+
